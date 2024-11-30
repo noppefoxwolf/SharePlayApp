b9518c007c2ee34d1f41ee03805b14b094920920
noppe <noppelabs@gmail.com>
2024-11-30 16:39:52 +0900

    Initial Commit

---
+++ SharePlayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharePlayApp
+//
+//  Created by Tomoya Hirano on 2024/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SharePlayAppTests/SharePlayAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SharePlayAppTests.swift
+//  SharePlayAppTests
+//
+//  Created by Tomoya Hirano on 2024/11/30.
+//
+
+import Testing
+@testable import SharePlayApp
+
+struct SharePlayAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
